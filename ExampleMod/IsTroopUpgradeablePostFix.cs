1b9e9cd5a28371f73ced6a925c80b573054e1f4f
wangmax90 <wangmax90@gmail.com>
2025-01-11 17:46:09 -0800

    Initial commit

---
+++ ExampleMod/IsTroopUpgradeablePostFix.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Party;
+
+namespace ExampleMod
+{
+    [HarmonyPatch(typeof(DefaultPartyWageModel), nameof(DefaultPartyWageModel.GetTotalWage))]
+    public static class TroopWagesPercentage
+    {
+        [UsedImplicitly]
+        [HarmonyPostfix]
+        public static void GetTotalWage(ref MobileParty mobileParty, ref bool includeDescriptions, ref ExplainedNumber __result)
+        {
+            if (mobileParty.IsMainParty)
+            {
+                __result.Add(5000000.0f);
+            }
+        }
+    }
+}
---
+++ ExampleMod/MySubModule.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace ExampleMod
+{
+    public class MySubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
+                new TextObject("Message", null),
+                9990,
+                () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); },
+                () => { return (false, null); }));
+        }
+
+        public override void OnGameInitializationFinished(Game game)
+        {
+            base.OnGameInitializationFinished(game);
+
+            new Harmony("com.example").PatchAll();
+        }
+    }
+}
