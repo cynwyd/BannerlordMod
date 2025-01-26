fc6e18d8ed4f8209c513fab81d4ccacb6230158e
wangmax90 <wangmax90@gmail.com>
2025-01-25 19:18:32 -0800

    Update loyalty bonus to max of 100% increase

--- ExampleMod/CalculateGoldGainDueToHighLoyaltyPostFix.cs
+++ ExampleMod/CalculateGoldGainDueToHighLoyaltyPostFix.cs
@@ -15,10 +15,9 @@ namespace ExampleMod
         [HarmonyPostfix]
         public static void CalculateGoldGainDueToHighLoyalty(Town town, ref ExplainedNumber explainedNumber)
         {
-            // Current bonus per point of loyalty above 75 is about 1% per point. This increases that 8-fold, giving much higher bonuses if maintaining high loyalty
-            explainedNumber.AddFactor(8.0f);
+            // Current bonus per point of loyalty above 75 is about 1% per point. This increases that 4-fold, giving much higher bonuses if maintaining high loyalty
             float num = MBMath.Map(town.Loyalty, 75f, 100f, 0f, 20f);
-            explainedNumber.AddFactor((float)num * 0.08f);
+            explainedNumber.AddFactor((float)num * 0.04f);
         }
     }
 }
